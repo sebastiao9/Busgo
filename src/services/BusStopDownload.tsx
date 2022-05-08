a0adb39fab2715d9ce8d0358466b4dac053685eb
Neto <ss.neto88@gmail.com>
2022-05-07 23:00:28 -0300

    fix: context correction

--- src/services/BusStopDownload.tsx
+++ src/services/BusStopDownload.tsx
@@ -1,37 +1,34 @@
 import { useEffect } from 'react';
-import AxiosContext from '../context/AxiosContext';
 import useAxios from '../hooks/useAxios';
 import { axiosRequestTypes } from '../types/axiosTypes';
 import DataContext from '../context/DataContext';
 
 const BusStopDownload = () => {
-  const { axiosData } = AxiosContext();
   const { dataContext } = DataContext();
   const { pickerOptions, setPickerOptions } = dataContext;
-  const { dataResponse } = axiosData;
-
-  const { getData } = useAxios();
 
   const dataPicker: axiosRequestTypes = {
     method: 'get',
     url: `busstop`,
   };
 
-  useEffect(() => {
-    getData(dataPicker);
-  }, []);
+  const { axiosRequest } = useAxios({
+    url: `busstop`,
+    method: `get`,
+  });
+
+  const { dataResponse, loading, erro } = axiosRequest;
 
   useEffect(() => {
-    pickerOptions &&
-      pickerOptions.length === 0 &&
-      dataResponse.map((item) => {
-        pickerOptions.some((value) => value.value === item.id)
-          ? null
-          : setPickerOptions((prev) => [
-              ...prev,
-              { label: item.name, value: item.id, lat: item.latitude, long: item.longitude },
-            ]);
-      });
+    dataResponse.map((item) => {
+      pickerOptions.some((value) => value.value === item.id)
+        ? null
+        : setPickerOptions((prev) => [
+            ...prev,
+            { label: item.name, value: item.id, lat: item.latitude, long: item.longitude },
+          ]);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dataResponse]);
 
   return {
