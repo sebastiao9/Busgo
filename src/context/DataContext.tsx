3eba0d1e9781e5a90812b8c82d5fe9db0ba220a7
Neto <ss.neto88@gmail.com>
2022-05-08 21:36:35 -0300

    fix: context correction

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -3,11 +3,13 @@ import { atom, useAtom } from 'jotai';
 const busStopList = atom([]);
 const busStopOptions = atom([]);
 const busStopSelelected = atom({});
+const pickerStatus = atom(false);
 
 const DataContext = () => {
-  const [busList, setBusList] = useAtom(busStopList);
-  const [pickerOptions, setPickerOptions] = useAtom(busStopOptions);
   const [selectedStop, setSelectedStop] = useAtom(busStopSelelected);
+  const [pickerOptions, setPickerOptions] = useAtom(busStopOptions);
+  const [busList, setBusList] = useAtom(busStopList);
+  const [pickerFilled, setPickerFilled] = useAtom(pickerStatus);
 
   return {
     dataContext: {
@@ -17,6 +19,8 @@ const DataContext = () => {
       setPickerOptions,
       selectedStop,
       setSelectedStop,
+      pickerFilled,
+      setPickerFilled,
     },
   };
 };
--- src/data/BusListDownload.tsx
+++ src/data/BusListDownload.tsx
@@ -1,28 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import useAxios from '../hooks/useAxios';
 import { axiosRequestTypes } from '../types/axiosTypes';
 import DataContext from '../context/DataContext';
 
 const BusListDownload = () => {
   const { dataContext } = DataContext();
-  const { busList, setBusList, selectedStop } = dataContext;
+  const { busList, setBusList, selectedStop, pickerFilled } = dataContext;
+  const { axiosRequest } = useAxios();
+  const { fetchData } = axiosRequest;
+  const { dataResponse, loading, erro } = axiosRequest;
 
   const listBusRequest: axiosRequestTypes = {
     method: 'get',
-    url: `busstop`,
+    url: `busstop/nextBuses/${selectedStop?.value}`,
   };
 
-  const { axiosRequest } = useAxios(listBusRequest);
-  const { dataResponse, loading, erro } = axiosRequest;
-
   useEffect(() => {
-    dataResponse.map((item) => {
-      console.log(item);
-    });
+    fetchData(listBusRequest);
+    pickerFilled && setBusList(dataResponse);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedStop]);
 
-  return {};
+  useEffect(() => {
+    console.log(busList);
+  }, [busList]);
+
+  return {
+    busList,
+  };
 };
 
 export default BusListDownload;
